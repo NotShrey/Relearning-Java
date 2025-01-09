b10c184b1a817a6039d5ca6c81fef8209e10944f
shreyas <shreyas.nayak.003@gmail.com>
2025-01-09 13:27:18 +0530

    first

---
+++ 1.java
@@ -0,0 +1,35 @@
+import java.lang.*;
+
+class DataSizeRange {
+    public static void main(String[] args) {
+        System.out.println("Integer Range:");
+        System.out.println("Min: " + Integer.MIN_VALUE);
+        System.out.println("Max: " + Integer.MAX_VALUE);
+
+        System.out.println("\nByte Range:");
+        System.out.println("Min: " + Byte.MIN_VALUE);
+        System.out.println("Max: " + Byte.MAX_VALUE);
+
+        System.out.println("\nShort Range:");
+        System.out.println("Min: " + Short.MIN_VALUE);
+        System.out.println("Max: " + Short.MAX_VALUE);
+
+        System.out.println("\nLong Range:");
+        System.out.println("Min: " + Long.MIN_VALUE);
+        System.out.println("Max: " + Long.MAX_VALUE);
+
+        System.out.println("\nFloat Range:");
+        System.out.println("Min: " + Float.MIN_VALUE);
+        System.out.println("Max: " + Float.MAX_VALUE);
+
+        System.out.println("\nDouble Range:");
+        System.out.println("Min: " + Double.MIN_VALUE);
+        System.out.println("Max: " + Double.MAX_VALUE);
+
+        int b = 6;
+        char c = 'z';
+        System.out.println(b);
+        System.out.println(c);
+
+    }
+}
