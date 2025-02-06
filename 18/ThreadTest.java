836e13f89549f493e4c0f3347ad2aec3457fadef
shreyas <shreyas.nayak.003@gmail.com>
2025-02-06 22:28:58 +0530

    Intro to Thread's

---
+++ 18/ThreadTest.java
@@ -0,0 +1,50 @@
+// class MyRun implements Thread{
+//     public void run() {
+//         int i = 1;
+//         while (i <= 20) {
+//             System.out.println(i + " Run");
+//             i++;
+//         }
+//     }
+// }
+
+
+class MyThread extends java.lang.Thread {
+
+    public void run() {
+        int i = 1;
+        while (i <= 20) {
+            System.out.println(i + " Yelo");
+            i++;
+        }
+    }
+}
+
+public class ThreadTest extends java.lang.Thread {
+
+    public void run() {
+        int i = 1;
+        while (i <= 20) {
+            System.out.println(i + " Mid");
+            i++;
+        }
+    }
+
+    public static void main(String[] args) {
+        MyThread m = new MyThread();
+        m.start();
+
+        ThreadTest t = new ThreadTest();
+        t.start();
+
+        // MyRun m = new MyRun();
+        // Thread th = new Thread(m);
+        // th.start();
+
+        int i = 1;
+        while (i <= 20) {
+            System.out.println(i + " Wrld");
+            i++;
+        }
+    }
+}
